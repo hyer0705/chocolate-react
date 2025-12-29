f0d368dcaae8f04240d9ba15f22b6d49dc1e9d4c
조혜린 <hyer1205@gmail.com>
2025-12-29 12:37:59 +0900

    feat: Link 태그와 useNavigate 훅을 사용하여 페이지 이동

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,13 +1,23 @@
 import "./App.css";
-import { Routes, Route } from "react-router";
+import { Routes, Route, Link, useNavigate } from "react-router";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
 
 function App() {
+  const nav = useNavigate();
+
   return (
     <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/new">New</Link>
+        <Link to="/diary">Diary</Link>
+      </div>
+      <div>
+        <button onClick={() => nav("/new")}>New로 이동</button>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
