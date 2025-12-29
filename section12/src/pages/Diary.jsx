0e0fcad4665ab51c94ec9df98a763cda83d78e93
조혜린 <hyer1205@gmail.com>
2025-12-29 12:41:57 +0900

    feat: 동적 라우팅, url params, query string
    - /:id
    - useParams() 훅
    - useSearchParams() 훅

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -1,5 +1,10 @@
+import { useParams } from "react-router";
+
 const Diary = () => {
-  return <h1>Diary!</h1>;
+  const { id } = useParams();
+  console.log(id);
+
+  return <h1>{id}번 일기!</h1>;
 };
 
 export default Diary;
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,4 +1,10 @@
+import { useSearchParams } from "react-router";
+
 const Home = () => {
+  const [searchParams] = useSearchParams();
+
+  console.log(searchParams.get("value"));
+
   return <h1>Home!</h1>;
 };
 
